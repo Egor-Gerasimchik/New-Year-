c8ec70f4f8505b2be63fdd84e7d1a945ae11f9f1
Egor-Gerasimchik <egor.gerasimchik2007@gmail.com>
2020-12-20 18:49:24 +0700

    jfa

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let music;
 $(document).ready(function () {
 
     music = new Howl({
-        src: ["https://ruv.hotmo.org/get/cuts/a8/0c/a80cb0f4f75a49ac7dfc2696be0b6e98/48261645/Christmas_Music_Christmas_Songs_Jingle_Bells_Christmas_Hits_Collective_-_Jingle_Bell_Rock_b128f0d132.mp3"],
+        src: ["audio/2.mp3", "audio/happy.mp3", "audio/3.mp3"],
         loop: true,
         volume: 0.1
 
@@ -14,12 +14,38 @@ $(document).ready(function () {
 
 });
 
-$("#player>img").click(function (e){
-    if(e.target.id != "isPlaying"){
-        music.play();
-        e.target.id ="isPlaying";
+$("#tree>img").click(function(e){ 
+    music.stop();
+    let name = e.target.id;
+    if(name == "toy1"){
+        music.play(1);
     }
-    
+    else if(name == "toy2"){
+        music.play(2);
+    }
+    else if(name == "toy3"){
+        music.play(3);
+    }
 });
 
+let timew = setInterval(function(){
+    let newYear = new Date("1 January 2021 00:00:00");
+    let now = new Date();
+    let diff = newYear - now;
+    console.log(diff);
 
+    let seconds = Math.floor(diff/1000);
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    let days = Math.floor(hours/24);
+
+    let days2 = hours % 24;
+    let hours2 = minutes % 60;
+    let minutes2 = seconds % 60;
+
+    $("#seconds").text(minutes2);
+    $("#minutes").text(hours2 + " :");
+    $("#hours").text(days2 + " :");
+    $("#days").text(days + " :");
+    
+}, 1000);
